49402165f08e29bfa1655060417060ff7d902172
201909 <74108873+201909@users.noreply.github.com>
2020-11-28 17:18:09 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,53 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var car,wall;
 
-var engine, world;
-var box1, box2;
-var ground
 
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
+var speed, weight; 
 
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(230,100,50,100);
-    ground = new Ground(200, 390, 400, 20)
+
+function setup() {
+  createCanvas(1600, 400);
+
+
+	speed=random(55,90)
+	weight=random(400,1500)
+
+
+	car=createSprite(50, 200, 50,50);   
+
+	car.velocityX = speed;
+
+	car.shapeColor=color(255);
+
+
+  
+  	wall=createSprite(1500,200, 60, height/2)
+  	wall.shapeColor=color(80,80,80)
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    box1.display();
-    box2.display();
-    ground.display();
+
+function draw() {
+  background(0);
+
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
+  
+  drawSprites();
+ 
 }
